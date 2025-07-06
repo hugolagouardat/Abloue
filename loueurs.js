a6d8f41660b4fa02e090cf1f0f52f414586abc0e
hugolagouardat <45795395+hugolagouardat@users.noreply.github.com>
2025-07-06 12:33:56 +0200

    Add files via upload

---
+++ loueurs.js
@@ -0,0 +1,20 @@
+const data = `
+﻿Loueur§Adresse§Departement§Commentaire§Ordre§Tel§Mail
+Alouer§83 Rue Nationale-Département 1§1§Disponible§1§0621080813§alouer@exemple.com
+BTP Loc§79 Rue de Paris-Département 1§1§Réservé§2§0645868471§btp_loc@exemple.com
+Batiloc§86 Avenue de la République-Département 1§1§Disponible§3§0671104139§batiloc@exemple.com
+Batiloc§194 Chemin des Dames-Département 1§1§À réviser§4§0633241705§batiloc@exemple.com
+Batiloc§178 Avenue de la République-Département 1§1§Ne pas utiliser§5§0681092332§batiloc@exemple.com
+Batiloc§186 Boulevard Haussmann-Département 1§1§Ne pas utiliser§6§0697306330§batiloc@exemple.com
+Alouer§81 Chemin des Dames-Département 2§2§Disponible§1§0677484236§alouer@exemple.com
+Alouer§51 Rue Nationale-Département 2§2§Disponible§2§0604564353§alouer@exemple.com
+Alouer§192 Rue de Paris-Département 2§2§Urgent§3§0679203019§alouer@exemple.com
+Alouer§87 Rue de Paris-Département 2§2§RAS§4§0635460986§alouer@exemple.com
+BTP Loc§5 Avenue de la République-Département 3§3§À réviser§1§0669302079§btp_loc@exemple.com
+BTP Loc§144 Rue de Paris-Département 3§3§Bon état§2§0699067249§btp_loc@exemple.com
+BTP Loc§9 Avenue de la République-Département 3§3§Ne pas utiliser§3§0694219082§btp_loc@exemple.com
+BTP Loc§81 Rue de Paris-Département 3§3§Client VIP§4§0695723047§btp_loc@exemple.com
+Batiloc§62 Chemin des Dames-Département 3§3§Réservé§5§0623186068§batiloc@exemple.com
+Batiloc§8 Rue de Paris-Département 3§3§Disponible§6§0625079629§batiloc@exemple.com
+§§§§§§
+`
